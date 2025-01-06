938c403032df124041344cb571117ddeba347bbb
nehamanghnani01 <nehajm@tamu.edu>
2025-01-06 04:42:19 -0600

    using library commands, modelfile, delete

--- start-2.py
+++ start-2.py
@@ -20,7 +20,34 @@ response = ollama.generate(
     prompt="what is the GDP of the United States?",
 )   
 
-print(response["response"])
-# for chunk in response:
-#     print(chunk["message"]["content"], end="", flush=True)
+# print(response["response"])
+for chunk in response:
+    print(chunk["message"]["content"], end="", flush=True)
+
+
+#show 
+ollama.show("llama3.2")
+
+modelfile = """
+FROM llama3.2
+SYSTEM You are a smart assists  that can answer questions and generate text based on the input you receive. Andswer succintly and with less verbose, to the point
+PARAMETER temperature 0.1
+"""
+
+ollama.create(model="temp_llama", modelfile=modelfile)
+
+response = ollama.chat(
+    model = "temp_llama", 
+    messages= [
+        {"role": "user", "content": "why is the ocean salty?"}
+    ], 
+    stream= True
+)
+
+# if stream is equal to True, the response has to be iterated over chunks
+for chunk in response:
+    print(chunk["message"]["content"], end="", flush=True)  
+
+ollama.delete("temp_llama")
+
 
